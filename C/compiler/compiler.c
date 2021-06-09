7a5d290f486fbf320e82fae87006376bc5c70f5f
Jero Bado <tokidokitalkyou@gmail.com>
2021-06-09 12:02:15 +0800

    Create the 'compiler' program
    Attempting to re-create the python command.

---
+++ C/compiler/compiler.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>   // bool data types
+
+int main()
+{
+    bool isTrue = true;
+
+    do {
+        printf("This is a test compiler.\n");
+    }
+    while (isTrue);
+    
+
+    return 0;
+}
+
