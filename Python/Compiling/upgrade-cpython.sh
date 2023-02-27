42697da7c13481d667eff498d7dfbeec4e48ee85
Jero Bado <programmer@jerobado.com>
2023-02-27 19:54:48 +0800

    Add usage and update script

--- Python/Compiling/upgrade-cpython.sh
+++ Python/Compiling/upgrade-cpython.sh
@@ -1,7 +1,14 @@
+# Usage
+# $ ./upgrade-cpython.sh <URL to python version>
+
+# For example:
+# $ ./upgrade-cpython.sh https://www.python.org/ftp/python/3.8.16/Python-3.8.16.tar.xz
+
 cd ~/Downloads
 wget $1
-tar -xf *.tar.gz
-cd cpython-*
+tar -xf *.tar.xz
+rm *.tar.xz
+cd Python-*
 ./configure --enable-optimizations --with-lto
 make -j4
 sudo make install
