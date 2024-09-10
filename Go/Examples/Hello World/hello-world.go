78d3e7004423beccad7a6d562cfa0ff0bf9963c3
Jero Bado <programmer@jerobado.com>
2024-09-10 09:19:04 +0800

    Add Examples directory
    This will contain all code sample on how to use the Go programming language.

---
+++ Go/Examples/Hello World/hello-world.go
@@ -0,0 +1,16 @@
+// How to run:
+// go run hello-world.go
+
+// How to build:
+// go build hello-world.go
+// ./hello-world
+
+package main
+
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+	fmt.Println(quote.Go())
+}
+
